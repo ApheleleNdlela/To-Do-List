8f76724cd1f33d8f444fcba5ba491ee11168221a
Aphelele_Ndlela <aphelelendlela95@gmail.com>
2023-08-21 13:17:52 +0200

    save function

--- Profile.js
+++ Profile.js
@@ -1,10 +1,18 @@
 const tbody = document.getElementById('tbody')
-// const myModal = document.getElementById('myModal')
-// const myInput = document.getElementById('myInput')
+const showbody = document.getElementById('showbody')
+const newItem = document.getElementById('newItem')
+const profile = document.getElementById('profile')
+const number = document.getElementById('number')
+const title = document.getElementById('title')
+const description = document.getElementById('description')
+const date = document.getElementById('date')
+const actions = document.getElementById('actions')
 
 const info = [];
 
-function profile() {
+let id = 0;
+
+function viewItem() {
 
 localStorage.setItem('info', JSON.stringify(info))
 JSON.parse(localStorage.getItem('info'))
@@ -25,28 +33,65 @@ rows+= `<tr>
 
 </tr>`
 
-tbody.innerHTML= rows
-profile()
+showbody.innerHTML= rows
+
 })
 
-}
-profile();
-
-function viewItem(){
-
-JSON.parse(localStorage.getItem(info))
-
 
 }
+viewItem();
+
+function showAddItem() {
+   newItem.style.display='block';
+   profile.style.display = 'none'
+
+
+}
+
+function cancel() {
+    window.location.href='profile.html'
+    
+}
+
+function save() {
+
+    id++;
+ const data ={
+    id: number.value,
+    title: title.value,
+    description: description.value,
+    date: date.value,
+    actions: actions.value
+ }
+
+}
+save()
+
+
     
 
 
 
 
 
+// function profile(){
 
+//     let x = JSON.parse(localStorage.getItem(info))
 
+//     info.map((data)=> {
+//         x+= `<tr>
+//         <td>${data.id}</td>
+//         <td>${data.title}</td>
+//         <td>${data.description}</td>
+//         <td>${data.date}</td>
+//         <td>
+//         <button>edit</button>
+//         <button>delete</button>
+//         </td>
+    
+//     </tr>`
+//     })
 
+//     info.push(x)
 
-
-
+// }
