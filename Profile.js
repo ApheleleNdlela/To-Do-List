fa3f611b43c1c2f24920573f13a94a7fe2e100f9
Aphelele_Ndlela <aphelelendlela95@gmail.com>
2023-08-23 14:10:27 +0200

    Delete and edit, edit doesn't work yet

--- Profile.js
+++ Profile.js
@@ -7,21 +7,26 @@ const title = document.getElementById('title-n')
 const description = document.getElementById('description-n')
 const date = document.getElementById('date-n')
 const actions = document.getElementById('actions')
+// const paragraph = document.getElementById("edit");
+// const edit = document.getElementById("edit-button");
+// const reset = document.getElementById("reset");
 
-const info = JSON.parse(localStorage.getItem('info'))?JSON.parse(localStorage.getItem('info')):[];
+let info = JSON.parse(localStorage.getItem('info'))?JSON.parse(localStorage.getItem('info')):[];
 
 let id = 0;
 
 let save = () => {
-
-   id++
     let object ={
        id: 0,
        title: title.value,
        description: description.value,
        date: date.value
    
+    } 
+    if(object.id == id){
+
     }
+    object.id = info.length
    
     info.push(object)
     localStorage.setItem('info', JSON.stringify(info))
@@ -35,34 +40,54 @@ let save = () => {
    
 //    save()
    
-info.sort((a , b)=> (a.id - b.id))
+// info.sort((a , b)=> (a.id - b.id))
  
 
 
 let viewItem =()=> {
 
-// let items = localStorage.eetItem('info', JSON.stringify(info))
+// let info = []
+// info = JSON.parse( localStorage.getItem('info'))
+
+
+if (info != null ){
+   info.reverse()
+   info.map((item,i) => {
+      let rows = "";
+      let itemElement = document.createElement("tr")
+      itemElement.innerHTML = `
+       <td>${++i}</td>
+       <td>${item.title}</td>
+       <td>${item.description}</td>
+       <td>${item.date}</td>
+       <td>
+       <button class="edit" type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal" data-bs-whatever="@mdo">edit</button>
+       <button type="button" class="btn btn-primary" onclick="del(${(item.id)})">delete</button>
+       </td>
+      `
+      showbody.appendChild(itemElement)
+      
+   });
+}
 
 
 
-let rows = "";
+// info.map((data)=> {
 
-info.map((data)=> {
+// rows+= `<tr>
+//     <td>${++item.id}</td>
+//     <td>${item.title}</td>
+//     <td>${item.description}</td>
+//     <td>${item.date}</td>
+//     <td>
+//     <button class="edit" type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal" data-bs-whatever="@mdo">edit</button>
+//     <button type="button" class="btn btn-primary" onclick="del(this)">delete</button>
+//     </td>
 
-rows+= `<tr>
-    <td>${++data.id}</td>
-    <td>${data.title}</td>
-    <td>${data.description}</td>
-    <td>${data.date}</td>
-    <td>
-    <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal" data-bs-whatever="@mdo">edit</button>
-    <button type="button" class="btn btn-primary">delete</button>
-    </td>
+// </tr>`
+// showbody.innerHTML= rows
 
-</tr>`
-showbody.innerHTML= rows
-
-})
+// })
 
 JSON.parse(localStorage.getItem('info'))
 }
@@ -84,10 +109,76 @@ function cancel() {
 
 
 
+function del(id) {
+   
 
+   info = info.filter(item => item.id !== id)
+   localStorage.setItem('info', JSON.stringify(info))
+   window.location.reload()
+   // alert('anything')
+
+   
+}
+
+// function edit(e){
+//    // let paragraph = document.querySelector("edit")
+  
+// //   let update = prompt("Update","showbody" );
+//   let a = showbody.parentElementSibling;
+//   e.parentElement[1].innerHTML = update
+//   console.log(e.parentElement.childNodes[0].innerHTML = update)
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// showbody.addEventListener('click', (event) =>{
+//    if(event.target.tagName === 'button'){
+//       const button = event.target
+//       const td = button.parentNode;
+//       const tr = td.parentNode;
+//       if(button.textContent === 'delete'){
+//          tr.removeChilNode(td);
+//       };
+//    }
+
+// })
+
+
+   
+   // tbody.addEventListener("click", function() {
+   //   showbody.contentEditable = true;
     
-
-
+   // } );
+   
+   // tbody.addEventListener("click", function() {
+   //   showbody.contentEditable = false;
+     
+   // } )
 
 
 
